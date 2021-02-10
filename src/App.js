d326d46cdd814192cf6917034d352c6919b7bc4e
imatos <iuriamatos@gmail.com>
2021-02-10 18:17:20 +0000

    fetch profiles from github

--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Spinner from './components/shared/Spinner';
+
+import './App.css';
 
 const App = () => {
-  const users = [
-    {
-      login: 'mojombo',
-      id: 1,
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    },
-    {
-      login: 'jamesgolick',
-      id: 37,
-      avatar_url: 'https://avatars.githubusercontent.com/u/37?v=4',
-      html_url: 'https://github.com/jamesgolick',
-    },
-    {
-      login: 'atmos',
-      id: 38,
-      avatar_url: 'https://avatars.githubusercontent.com/u/38?v=4',
-      html_url: 'https://github.com/atmos',
-    },
-    {
-      login: 'errfree',
-      id: 44,
-      avatar_url: 'https://avatars.githubusercontent.com/u/44?v=4',
-      html_url: 'https://github.com/errfree',
-    },
-    {
-      login: 'mojodna',
-      id: 45,
-      avatar_url: 'https://avatars.githubusercontent.com/u/45?v=4',
-      html_url: 'https://github.com/mojodna',
-    },
-    {
-      login: 'bmizerany',
-      id: 46,
-      avatar_url: 'https://avatars.githubusercontent.com/u/46?v=4',
-      html_url: 'https://github.com/bmizerany',
-    },
-  ];
+  const [users, setUsers] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoaded(true);
+        setUsers(data);
+      });
+  }, []);
 
   return (
     <>
       <Navbar title="Github Profile Finder" icon="fab fa-github" />
       <div className="container">
-        <Users users={users} />
+        {isLoaded ? <Users users={users} /> : <Spinner />}
       </div>
     </>
   );
